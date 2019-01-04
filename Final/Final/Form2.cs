473f1217dbe2a394f74f35aa0a7898407fccbe8e
Sam Olsen <mr.awesome313131@gmail.com>
2019-01-04 13:14:30 -0600

    friday push

---
+++ Final/Final/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final
+{
+    public partial class mainMenu : Form
+    {
+        public mainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1PlayerCpu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn2PlayerGame_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
